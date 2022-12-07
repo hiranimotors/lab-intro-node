159d7ec4d8ee9cc266c7d3cd1f37d7322fbca10a
omarali <o.ali94@gmail.com>
2022-12-07 21:15:02 +0000

    solved lab

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    return this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    const sumOfItems = this.items.reduce((a, c) => a + c, 0);
+    return sumOfItems;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
 
-console.log(5);
+const oddNumbers = new SortedList();
+
+oddNumbers.add(5);
+oddNumbers.add(57);
+oddNumbers.add(543);
+oddNumbers.add(99);
+
+console.log(oddNumbers.items);
+console.log(oddNumbers);
